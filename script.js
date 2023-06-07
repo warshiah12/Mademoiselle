e7145527d81213bfc87341cf65a28483e869f265
warshiah12 <91490210+warshiah12@users.noreply.github.com>
2023-06-07 08:17:56 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+﻿//handling the hamburger menu
+let navBar = document.querySelector('#navLinks');
+let menuBar = document.querySelector('.ham-menu');
+
+menuBar.onclick = () => {
+    menuBar.classList.toggle('fa-solid.fa-bars');
+    navBar.classList.toggle('show');
+}
+
+//function barFunction() {
+//    var links = document.getElementById("navLinks");
+//    if (links.style.display === "block") {
+//        links.style.display = "none";
+//    }
+//    else {
+//        links.style.display = "block";
+//    }
+//}
